bc3fa0b2b1dda9b94fac823f52159c4ca7e33f25
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-07-21 14:25:41 -0300

    .

--- emprestimo.py
+++ emprestimo.py
@@ -1,11 +1,13 @@
-emprestimo = float(input('Digite o valor do emprestimo: R$'))
+emprestimo = float(input('Digite o valor do empréstimo: R$'))
 salario = float(input('Quanto você ganha? R$'))
-anos = int(input('Quantos anos de financiamento? R$'))
+anos = int(input('Quantos anos de financiamento?'))
 prestacao = emprestimo / (anos * 12)
 minimo = salario * 30 / 100
 
-print(f'Para pagar uma emprestimo de R${emprestimo} em {anos}anos')
-print(f'A prestação sera de R${prestacao}')
+print(f'Para pagar uma empréstimo de R${emprestimo} em {anos} anos')
+print(f'A prestação sera de R${prestacao:.2f}')
 
 if prestacao <= minimo:
-    
+    print('Empréstimo pode ser CONCEDIDO!!')
+else:
+    print('Empréstimo NEGADO!!')    
